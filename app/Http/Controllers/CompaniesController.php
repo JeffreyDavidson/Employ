a7c961f1cba2142e3638d9e4f598d802057003e2
Jeffrey Davidson <jeffrey.davidson@outlook.com>
2019-04-24 13:04:53 -0400

    Add missing docblocks

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -6,12 +6,17 @@ use App\Company;
 
 class CompaniesController extends Controller
 {
+    /**
+     * Retrieve companies from system.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
         $this->authorize('viewList', Company::class);
 
         $companies = Company::paginate(10);
-        
+
         return view('companies.index', compact('companies'));
     }
 }
